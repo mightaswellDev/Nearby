d6a3a86ff8753676b011bac9b814fcee2b47e631
cchaison0 <cchaison0@might-as-well.com>
2014-01-24 18:41:47 -0700

    Removed Dummy content and added surface view

--- src/com/mightaswell/nearby/FullscreenActivity.java
+++ src/com/mightaswell/nearby/FullscreenActivity.java
@@ -46,8 +46,8 @@ public class FullscreenActivity extends Activity {
 
 		setContentView(R.layout.activity_fullscreen);
 
-		final View controlsView = findViewById(R.id.fullscreen_content_controls);
-		final View contentView = findViewById(R.id.fullscreen_content);
+		
+		final View contentView = findViewById(R.id.surfaceView1);
 
 		// Set up an instance of SystemUiHider to control the system UI for
 		// this activity.
@@ -68,23 +68,12 @@ public class FullscreenActivity extends Activity {
 							// (Honeycomb MR2 and later), use it to animate the
 							// in-layout UI controls at the bottom of the
 							// screen.
-							if (mControlsHeight == 0) {
-								mControlsHeight = controlsView.getHeight();
-							}
+							
 							if (mShortAnimTime == 0) {
 								mShortAnimTime = getResources().getInteger(
 										android.R.integer.config_shortAnimTime);
 							}
-							controlsView
-									.animate()
-									.translationY(visible ? 0 : mControlsHeight)
-									.setDuration(mShortAnimTime);
-						} else {
-							// If the ViewPropertyAnimator APIs aren't
-							// available, simply show or hide the in-layout UI
-							// controls.
-							controlsView.setVisibility(visible ? View.VISIBLE
-									: View.GONE);
+							
 						}
 
 						if (visible && AUTO_HIDE) {
@@ -95,22 +84,12 @@ public class FullscreenActivity extends Activity {
 				});
 
 		// Set up the user interaction to manually show or hide the system UI.
-		contentView.setOnClickListener(new View.OnClickListener() {
-			@Override
-			public void onClick(View view) {
-				if (TOGGLE_ON_CLICK) {
-					mSystemUiHider.toggle();
-				} else {
-					mSystemUiHider.show();
-				}
-			}
-		});
+		
 
 		// Upon interacting with UI controls, delay any scheduled hide()
 		// operations to prevent the jarring behavior of controls going away
 		// while interacting with the UI.
-		findViewById(R.id.dummy_button).setOnTouchListener(
-				mDelayHideTouchListener);
+		
 	}
 
 	@Override
